99297a563d3775971c7368728e1311c132ecf6f1
mityakoval <dima.alex.koval@gmail.com>
2017-03-31 12:46:15 +0200

    fix 3d tour on mobile, add links to instagram and facebook

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def index
     puts request.user_agent
     @mobile = (request.user_agent.include?('iPhone') || request.user_agent.include?('Android'))
-    puts @mobile
+    @ipad = request.user_agent.include?('iPad')
     @categories = Category.all
   end
 end
